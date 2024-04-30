9663684c342c239fb4d4f084ef5f83f01fe3bf19
Burn Pc <seraphim@escsoft.de>
2024-04-30 08:48:56 +0200

    Init Rc

--- AnimeCoverImage/Services/IAnimeCoverImage.cs
+++ AnimeCoverImage/Services/IAnimeCoverImage.cs
@@ -1,12 +1,12 @@
 ﻿namespace AnimeCoverImage.Services
 {
-    public  interface IAnimeCoverImage
+    public interface IAnimeCoverImage
     {
         /// <summary>
         /// 
         /// </summary>
         /// <param name="name">Animename</param>
         /// <returns>Picture Url</returns>
-        public Task<Dictionary<string,string>> GetAnimeCoverAsync(string name);
+        public Task<Dictionary<string, string>> GetAnimeCoverAsync(string name);
     }
 }
